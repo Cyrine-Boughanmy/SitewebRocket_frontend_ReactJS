6f933f0d6a6ce2cea10b57a8210568de7a2d2013
Cyrine <boughanmycyrine@gmail.com>
2022-07-22 13:53:30 +0100

    cleaning code

--- src/components/homepage/formez-vous/formezvous.js
+++ src/components/homepage/formez-vous/formezvous.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import './formezvous.css';
 import Section from '../../section/section';
 import { formezvous } from '../pages';
 const Formezvous = () => {
@@ -14,49 +8,5 @@ const Formezvous = () => {
     </>
   );
 }
-
-    {/*
-        <React.Fragment>
-      <CssBaseline />
-      <Box component="div" sx={{backgroundColor:'#FFFF', display: 'flex', height:'100vh'}}>
-        
-      
-      <Grid container spacing={2} sx={{marginTop:'100px'}}>
-        
-          <Grid item xs={12}  md={6} >
-          <Box
-          sx={{display: 'flex',overflow:'hidden',backgroundColor:'transparent', justifyContent:'flex-start',marginTop:'20px'}}
-          >
-        <img src={fvimg} alt=''/>
-          </Box>
-        </Grid>
-        
-      
- 
-        
-        <Grid item xs={12} md={6}>
-
-        
-              <h1 className='text-titlel'>
-              Formez-vous au métier de 
-              Développeur Full Stack <br></br> 
-              Web et Mobile
-              </h1>
-                <p className='text-descriptionl'>
-                Cette formation à temps plein, intensive<br></br>
-                d’une durée de dix semaines, vous offre la <br></br>
-                possibilté de vous former depuis chez vous<br></br>
-                au métier de Développeur Full Stack
-                </p>
-                <button className='btn-infosessionf'>
-                Paypal                </button>
-            
-        
-        </Grid>
-        
-      </Grid>
-    
-      </Box>    
-  </React.Fragment> */}
 
 export default Formezvous;
--- src/components/homepage/pages.js
+++ src/components/homepage/pages.js
@@ -40,16 +40,4 @@ export const header = {
     img: prqfaire,
     alt: 'pourquoi faire le Bootcamp'
   };
-  {/*}
-  export const homeObjFour = {
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
-    headline: 'Every transaction is stored on our secure cloud database',
-    description:
-      'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
-    buttonLabel: 'Sign Up Now',
-    imgStart: 'start',
-    img: 'images/svg-8.svg',
-    alt: 'Vault'
-  }; */}
+  
